adb07222c78a687b0bf337a77d29eb1da08604c5
GhoulBoii <78494833+GhoulBoii@users.noreply.github.com>
2022-10-31 20:08:55 +0530

    Adding Files

---
+++ config.h
@@ -0,0 +1,20 @@
+#include "/home/ghoul/.cache/wal/colors-wal-dmenu.h"
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static char *fonts[] =
+{
+	"FiraCode Nerd Font:size=15", "Noto Color Emoji:autohint=true"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
